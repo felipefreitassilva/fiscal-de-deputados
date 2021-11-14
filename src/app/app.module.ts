c0621ad891f40e07f5f43492104fd610417e073f
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2021-11-14 11:31:18 -0300

    Static Build
    Created routers, header and home component, added basic styling, added base_url and custom components and methods. Prepared for API fetching - works on console

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { GaugeModule } from 'angular-gauge';
-import {MatTabsModule} from '@angular/material/tabs'
-import {MatIconModule} from '@angular/material/icon'
-import {MatFormFieldModule} from '@angular/material/form-field'
-import {MatSelectModule} from '@angular/material/select'
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatIconModule } from '@angular/material/icon'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import {MatSelectModule} from '@angular/material/select';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { HomeComponent } from './components/home/home.component'
+import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +33,18 @@ import {MatSelectModule} from '@angular/material/select'
     MatFormFieldModule,
     MatSelectModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi:true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi:true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
